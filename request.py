97bc4f0df6fee3e68e5af6fd99e201f4ea8074da
unknown <deviarrais34@gmail.com>
2022-10-13 13:48:24 -0300

    Atualizar o capitulo novo no banco de dados

--- request.py
+++ request.py
@@ -2,22 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-conn = sqlite3.connect('mangas.db')
-cur = conn.cursor()
 
 def verificaCapitulo(link, n):
+    conn = sqlite3.connect('mangas.db')
+    cur = conn.cursor()
 
-    mangaKalot = requests.get(f'{link}').content
-    soup = BeautifulSoup(mangaKalot, 'html.parser')
-    lista = soup.li.a
-    chapter = lista.text
-    oldCapitulo = cur.execute(f"SELECT cap FROM capitulo WHERE id={n}")
+    try:
+        mangaKalot = requests.get(f'{link}').content
+        soup = BeautifulSoup(mangaKalot, 'html.parser')
+        lista = soup.li.a
+        chapter = lista.text
+    except:
+        print("não foi possivel conectar ao site")
+    oldCapitulo = cur.execute(f"SELECT cap FROM capitulo WHERE id={n};")
     capitulo = cur.fetchall()
     capitulos = capitulo[0][0]
     if capitulos == chapter:
         pass
     else:
-        title = cur.execute(f"SELECT titulo FROM capitulo WHERE id={n}")
+        title = cur.execute(f"SELECT titulo FROM capitulo WHERE id={n};")
         titles = cur.fetchall()
         titulo = titles[0][0]
+        cur.execute(f"UPDATE capitulo SET cap = '{chapter}' WHERE id={n};")
+        conn.commit()
         print(titulo)
